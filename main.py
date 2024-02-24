89fa52f402b747db7a6b0b5c0042fc0828508f02
sandens <sandens@gmail.com>
2024-02-24 02:01:59 -0500

    updated endpoint

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def redistest():
     import redis
     logger.info("Testing Redis")
     redis_pw = os.environ["REDIS_PASSWORD"]
-    r = redis.Redis(host='mydb-redis-master.appworkloads.svc.cluster.local', port=6379, db=0,password=redis_pw)
+    r = redis.Redis(host='appworkloads-redis-master.appworkloads.svc.cluster.local', port=6379, db=0,password=redis_pw)
     logger.info(f"Connected to Redis: {r}")
     key = str(uuid.uuid4())
     logger.info(f"Setting date for key {key}")
